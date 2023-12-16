7988cc68e2d56ce283ee721119fabb5f0f1efcee
Heathcliff <heathcliff@heathcliff.eu>
2023-12-16 22:58:10 +0100

    speedtest-exporter: Add option to push metrics to prometheus directly
    This allows to push metrics directly to prometheus by implementing it's
    remote_write api.
    Additionally to manage the increased need for flags, switch to config file
    instead.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- apps/speedtest-exporter/cmd/main.go
+++ apps/speedtest-exporter/cmd/main.go
@@ -1,27 +1,31 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/heathcliff26/containers/apps/speedtest-exporter/pkg/collector"
+	"github.com/heathcliff26/containers/apps/speedtest-exporter/pkg/config"
+	"github.com/heathcliff26/containers/apps/speedtest-exporter/pkg/promremote"
 	"github.com/heathcliff26/containers/apps/speedtest-exporter/pkg/speedtest"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var logLevel *slog.LevelVar
+var (
+	configPath string
+	env        bool
+)
 
 // Initialize the logger
 func init() {
-	logLevel = &slog.LevelVar{}
-	opts := slog.HandlerOptions{
-		Level: logLevel,
-	}
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &opts))
-	slog.SetDefault(logger)
+	flag.StringVar(&configPath, "config", "", "Optional: Path to config file")
+	flag.BoolVar(&env, "env", false, "Used together with -config, when set will expand enviroment variables in config")
 }
 
 // Handle requests to the webroot.
@@ -32,37 +36,69 @@ func ServerRootHandler(w http.ResponseWriter, r *http.Request) {
 
 func createSpeedtest(path string) (speedtest.Speedtest, error) {
 	if path == "" {
+		slog.Debug("Using go-native speedtest implementation")
 		return speedtest.NewSpeedtest(), nil
 	} else {
+		slog.Debug("Using external speedtest-cli binary", "path", path)
 		return speedtest.NewSpeedtestCLI(path)
 	}
 }
 
 func main() {
-	parseFlags()
+	flag.Parse()
 
-	s, err := createSpeedtest(speedtestPath)
+	cfg, err := config.LoadConfig(configPath, env)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed initialize speedtest: %v\n", err)
+		slog.Error("Could not load configuration", slog.String("path", configPath), slog.String("err", err.Error()))
 		os.Exit(1)
 	}
 
-	collector, err := collector.NewCollector(cacheDuration, instance, s)
+	s, err := createSpeedtest(cfg.SpeedtestCLI)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed create collector: %v\n", err)
+		slog.Error("Failed initialize speedtest", "err", err)
+		os.Exit(1)
+	}
+
+	collector, err := collector.NewCollector(cfg.Cache, s)
+	if err != nil {
+		slog.Error("Failed to create collector", "err", err)
 		os.Exit(1)
 	}
 
 	reg := prometheus.NewRegistry()
 	reg.MustRegister(collector)
 
+	if cfg.Remote.Enable {
+		rwClient, err := promremote.NewWriteClient(cfg.Remote.URL, cfg.Remote.Instance, "integrations/speedtest", reg)
+		if err != nil {
+			slog.Error("Failed to create remote write client", "err", err)
+			os.Exit(1)
+		}
+		if cfg.Remote.Username != "" {
+			err := rwClient.SetBasicAuth(cfg.Remote.Username, cfg.Remote.Password)
+			if err != nil {
+				slog.Error("Failed to create remote_write client", "err", err)
+				os.Exit(1)
+			}
+		}
+
+		slog.Info("Starting remote_write client")
+		rwQuit := make(chan bool)
+		rwClient.Run(cfg.Cache, rwQuit)
+		defer func() {
+			rwQuit <- true
+			close(rwQuit)
+		}()
+	}
+
 	http.HandleFunc("/", ServerRootHandler)
 	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
 
+	addr := ":" + strconv.Itoa(cfg.Port)
 	slog.Info("Starting http server", slog.String("addr", addr))
 	err = http.ListenAndServe(addr, nil)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to start http server: %v\n", err)
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("Failed to start http server", "err", err)
 		os.Exit(1)
 	}
 }
--- apps/speedtest-exporter/pkg/collector/collector_test.go
+++ apps/speedtest-exporter/pkg/collector/collector_test.go
@@ -9,32 +9,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var fakeSpeedtestResultSuccess = speedtest.NewSpeedtestResult(0.5, 15, 876.53, 12.34, 950.3079, "Foo Corp.", "127.0.0.1")
+var mockSpeedtestResult = speedtest.NewSpeedtestResult(0.5, 15, 876.53, 12.34, 950.3079, "Foo Corp.", "127.0.0.1")
 
-type FakeSpeedtest struct {
-	callback func()
-	fail     bool
-}
-
-func (s *FakeSpeedtest) Speedtest() *speedtest.SpeedtestResult {
-	if s.callback != nil {
-		s.callback()
-	}
-	if s.fail {
-		return speedtest.NewFailedSpeedtestResult()
-	}
-	return fakeSpeedtestResultSuccess
+func NewMockSpeedtest() *speedtest.MockSpeedtest {
+	return &speedtest.MockSpeedtest{Result: mockSpeedtestResult}
 }
 
 func TestNewCollector(t *testing.T) {
-	s := &FakeSpeedtest{}
+	s := NewMockSpeedtest()
 	expectedCollector := &Collector{
 		cacheTime: 5,
-		instance:  "foo",
 		speedtest: s,
 	}
 
-	actualCollector, err := NewCollector(5, "foo", s)
+	actualCollector, err := NewCollector(5, s)
 	if err != nil {
 		t.Fatalf("Could not create new Collector: %v", err)
 	}
@@ -43,12 +31,12 @@ func TestNewCollector(t *testing.T) {
 
 	assert.Equal(expectedCollector, actualCollector)
 
-	_, err = NewCollector(0, "", nil)
+	_, err = NewCollector(0, nil)
 	assert.Equal(ErrNoSpeedtest{}, err)
 }
 
 func TestSetNextSpeedtestTime(t *testing.T) {
-	c, err := NewCollector(5, "foo", &FakeSpeedtest{})
+	c, err := NewCollector(5, NewMockSpeedtest())
 	if err != nil {
 		t.Fatalf("Could not create new Collector: %v", err)
 	}
@@ -60,7 +48,7 @@ func TestSetNextSpeedtestTime(t *testing.T) {
 }
 
 func TestFirstSpeedtestRun(t *testing.T) {
-	c, err := NewCollector(5, "foo", &FakeSpeedtest{})
+	c, err := NewCollector(5, NewMockSpeedtest())
 	if err != nil {
 		t.Fatalf("Could not create new Collector: %v", err)
 	}
@@ -68,20 +56,19 @@ func TestFirstSpeedtestRun(t *testing.T) {
 
 	assert := assert.New(t)
 
-	assert.Equal(fakeSpeedtestResultSuccess, result)
+	assert.Equal(mockSpeedtestResult, result)
 	assert.Equal(result, c.lastResult)
 	assert.NotEmpty(c.nextSpeedtest)
 }
 
 func TestResultFromCache(t *testing.T) {
 	speedtestRan := false
-	s := &FakeSpeedtest{
-		callback: func() {
-			speedtestRan = true
-		},
+	s := NewMockSpeedtest()
+	s.Callback = func() {
+		speedtestRan = true
 	}
 
-	c, err := NewCollector(5, "foo", s)
+	c, err := NewCollector(5, s)
 	if err != nil {
 		t.Fatalf("Could not create new Collector: %v", err)
 	}
@@ -101,13 +88,12 @@ func TestResultFromCache(t *testing.T) {
 
 func TestRunSpeedtestWhenCacheEmpty(t *testing.T) {
 	speedtestRan := false
-	s := &FakeSpeedtest{
-		callback: func() {
-			speedtestRan = true
-		},
+	s := NewMockSpeedtest()
+	s.Callback = func() {
+		speedtestRan = true
 	}
 
-	c, err := NewCollector(5, "foo", s)
+	c, err := NewCollector(5, s)
 	if err != nil {
 		t.Fatalf("Could not create new Collector: %v", err)
 	}
@@ -119,7 +105,7 @@ func TestRunSpeedtestWhenCacheEmpty(t *testing.T) {
 	assert := assert.New(t)
 
 	assert.NotEmpty(result)
-	assert.Equal(fakeSpeedtestResultSuccess, result)
+	assert.Equal(mockSpeedtestResult, result)
 	assert.Equal(result, c.lastResult)
 	if !speedtestRan {
 		t.Error("Speedtest was not called")
@@ -128,13 +114,12 @@ func TestRunSpeedtestWhenCacheEmpty(t *testing.T) {
 
 func TestRunSpeedtestWhenCacheExpired(t *testing.T) {
 	speedtestRan := false
-	s := &FakeSpeedtest{
-		callback: func() {
-			speedtestRan = true
-		},
+	s := NewMockSpeedtest()
+	s.Callback = func() {
+		speedtestRan = true
 	}
 
-	c, err := NewCollector(5, "foo", s)
+	c, err := NewCollector(5, s)
 	if err != nil {
 		t.Fatalf("Could not create new Collector: %v", err)
 	}
@@ -147,7 +132,7 @@ func TestRunSpeedtestWhenCacheExpired(t *testing.T) {
 
 	assert.NotEmpty(result)
 	assert.NotEqual(speedtest.NewFailedSpeedtestResult(), result)
-	assert.Equal(fakeSpeedtestResultSuccess, result)
+	assert.Equal(mockSpeedtestResult, result)
 	assert.Equal(result, c.lastResult)
 	if !speedtestRan {
 		t.Error("Speedtest was not called")
@@ -161,15 +146,14 @@ func TestSpeedtestIsNotRunConcurrently(t *testing.T) {
 
 	i := 0
 	sleeping := make(chan bool, 1)
-	s := &FakeSpeedtest{
-		callback: func() {
-			i++
-			sleeping <- true
-			time.Sleep(10 * time.Second)
-		},
+	s := NewMockSpeedtest()
+	s.Callback = func() {
+		i++
+		sleeping <- true
+		time.Sleep(10 * time.Second)
 	}
 
-	c, err := NewCollector(5, "foo", s)
+	c, err := NewCollector(5, s)
 	if err != nil {
 		t.Fatalf("Could not create new Collector: %v", err)
 	}
@@ -191,8 +175,8 @@ func TestSpeedtestIsNotRunConcurrently(t *testing.T) {
 }
 
 func TestCollect(t *testing.T) {
-	s := &FakeSpeedtest{}
-	c, err := NewCollector(0, "foo", s)
+	s := NewMockSpeedtest()
+	c, err := NewCollector(0, s)
 	if err != nil {
 		t.Fatalf("Could not create new Collector: %v", err)
 	}
@@ -201,39 +185,39 @@ func TestCollect(t *testing.T) {
 		ch := make(chan prometheus.Metric, 1)
 		go c.Collect(ch)
 
-		actualLabelValues := []string{c.instance, fakeSpeedtestResultSuccess.ClientIp(), fakeSpeedtestResultSuccess.ClientIsp()}
+		actualLabelValues := []string{mockSpeedtestResult.ClientIp(), mockSpeedtestResult.ClientIsp()}
 
 		actualMetric := <-ch
-		expectedMetric := prometheus.MustNewConstMetric(jitterLatencyDesc, prometheus.GaugeValue, fakeSpeedtestResultSuccess.JitterLatency(), actualLabelValues...)
+		expectedMetric := prometheus.MustNewConstMetric(jitterLatencyDesc, prometheus.GaugeValue, mockSpeedtestResult.JitterLatency(), actualLabelValues...)
 		assert.Equal(t, expectedMetric, actualMetric)
 
 		actualMetric = <-ch
-		expectedMetric = prometheus.MustNewConstMetric(pingDesc, prometheus.GaugeValue, fakeSpeedtestResultSuccess.Ping(), actualLabelValues...)
+		expectedMetric = prometheus.MustNewConstMetric(pingDesc, prometheus.GaugeValue, mockSpeedtestResult.Ping(), actualLabelValues...)
 		assert.Equal(t, expectedMetric, actualMetric)
 
 		actualMetric = <-ch
-		expectedMetric = prometheus.MustNewConstMetric(downloadSpeedDesc, prometheus.GaugeValue, fakeSpeedtestResultSuccess.DownloadSpeed(), actualLabelValues...)
+		expectedMetric = prometheus.MustNewConstMetric(downloadSpeedDesc, prometheus.GaugeValue, mockSpeedtestResult.DownloadSpeed(), actualLabelValues...)
 		assert.Equal(t, expectedMetric, actualMetric)
 
 		actualMetric = <-ch
-		expectedMetric = prometheus.MustNewConstMetric(uploadSpeedDesc, prometheus.GaugeValue, fakeSpeedtestResultSuccess.UploadSpeed(), actualLabelValues...)
+		expectedMetric = prometheus.MustNewConstMetric(uploadSpeedDesc, prometheus.GaugeValue, mockSpeedtestResult.UploadSpeed(), actualLabelValues...)
 		assert.Equal(t, expectedMetric, actualMetric)
 
 		actualMetric = <-ch
-		expectedMetric = prometheus.MustNewConstMetric(dataUsedDesc, prometheus.GaugeValue, fakeSpeedtestResultSuccess.DataUsed(), actualLabelValues...)
+		expectedMetric = prometheus.MustNewConstMetric(dataUsedDesc, prometheus.GaugeValue, mockSpeedtestResult.DataUsed(), actualLabelValues...)
 		assert.Equal(t, expectedMetric, actualMetric)
 
 		actualMetric = <-ch
-		expectedMetric = prometheus.MustNewConstMetric(upDesc, prometheus.GaugeValue, 1, c.instance)
+		expectedMetric = prometheus.MustNewConstMetric(upDesc, prometheus.GaugeValue, 1)
 		assert.Equal(t, expectedMetric, actualMetric)
 	})
 
 	t.Run("Failure", func(t *testing.T) {
 		ch := make(chan prometheus.Metric, 1)
-		s.fail = true
+		s.Fail = true
 		go c.Collect(ch)
 		actualMetric := <-ch
-		expectedMetric := prometheus.MustNewConstMetric(upDesc, prometheus.GaugeValue, 0, c.instance)
+		expectedMetric := prometheus.MustNewConstMetric(upDesc, prometheus.GaugeValue, 0)
 		assert.Equal(t, expectedMetric, actualMetric)
 	})
 }
