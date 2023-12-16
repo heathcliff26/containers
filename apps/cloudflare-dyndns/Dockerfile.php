e29f1e6b8096a65136c54b9efd65ada387e417c8
bot-ahsoka <noreply@heathcliff.eu>
2023-12-16 12:34:25 +0000

    chore(container): update image docker.io/library/php to 110e09c

--- apps/cloudflare-dyndns/Dockerfile.php
+++ apps/cloudflare-dyndns/Dockerfile.php
@@ -1,3 +1,3 @@
-FROM docker.io/library/php:8.3.0-apache-bookworm@sha256:f51a5168c4832b923fc22a52f1ef5475cdd02333c8efd924e5c4f773000fb0fc
+FROM docker.io/library/php:8.3.0-apache-bookworm@sha256:110e09c94d7a1962e4b9c334300a3cced6c2578c87c98fd08ddf93647db0e22e
 
 COPY fritzbox_dyndns.php /var/www/html/index.php
