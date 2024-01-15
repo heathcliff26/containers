a57ca3fa43d7c7825a4c395223ff9c6a6d9ac56b
Heathcliff <heathcliff@heathcliff.eu>
2024-01-15 21:20:03 +0100

    minecraft-exporter: Replace fmt.Errorf with custom error types
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- apps/minecraft-exporter/pkg/rcon/errors.go
+++ apps/minecraft-exporter/pkg/rcon/errors.go
@@ -1,5 +1,7 @@
 package rcon
 
+import "fmt"
+
 type ErrRCONMissingHost struct{}
 
 func (e ErrRCONMissingHost) Error() string {
@@ -22,4 +24,26 @@ type ErrRCONConnectionTimeout struct{}
 
 func (e ErrRCONConnectionTimeout) Error() string {
 	return "Timed out waiting for a response"
+}
+
+type ErrForgeTPS struct{}
+
+func (e ErrForgeTPS) Error() string {
+	return "Failed to retrieve the overall tps stats"
+}
+
+type ErrPaperTPS struct {
+	Text  string
+	Count int
+}
+
+func NewErrPaperTPS(text string, count int) error {
+	return &ErrPaperTPS{
+		Text:  text,
+		Count: count,
+	}
+}
+
+func (e *ErrPaperTPS) Error() string {
+	return fmt.Sprintf("Expected at 3 values, got %d. Input: \"%s\"", e.Count, e.Text)
 }
