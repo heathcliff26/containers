97eba0c0e9963f392ef024b0a35f3b0f4d0806be
Heathcliff <heathcliff@heathcliff.eu>
2024-01-19 15:42:20 +0100

    Migrate speedtest-exporter to it's own repository
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- apps/speedtest-exporter/pkg/collector/errors.go
+++
@@ -1,7 +0,0 @@
-package collector
-
-type ErrNoSpeedtest struct{}
-
-func (e ErrNoSpeedtest) Error() string {
-	return "No valid speedtest provided"
-}
--- apps/speedtest-exporter/pkg/speedtest/testdata/speedtest-cli.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-base_dir="$(dirname "${0}" | xargs realpath)"
-
-cat "${base_dir}/speedtest-cli-result.json"
