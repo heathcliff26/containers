bea51e80500060dfa9230adf063269be6ab6a74d
Heathcliff <heathcliff@heathcliff.eu>
2024-08-07 21:14:35 +0200

    Add fcos-k8s container
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

---
+++ apps/fcos-k8s/k8s-install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -ex
+
+# First the (modified) steps from:
+# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+
+KUBERNETES_SHORT_VERSION="${KUBERNETES_VERSION%.*}"
+
+# This overwrites any existing configuration in /etc/yum.repos.d/kubernetes.repo
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_SHORT_VERSION}/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_SHORT_VERSION}/rpm/repodata/repomd.xml.key
+EOF
+
+rpm-ostree install "kubelet-${KUBERNETES_VERSION}" "kubeadm-${KUBERNETES_VERSION}" "kubectl-${KUBERNETES_VERSION}" cri-o
+rpm-ostree cleanup -m
+
+systemctl enable kubelet.service crio.service
+
+# Now the steps from:
+# https://docs.fedoraproject.org/en-US/quick-docs/using-kubernetes/
+
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+net.ipv6.conf.all.forwarding        = 1
+EOF
+
+ostree container commit
