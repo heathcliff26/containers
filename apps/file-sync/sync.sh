e3c6d109049c2596d36a85a1a5feaa184411b958
Heathcliff <heathcliff@heathcliff.eu>
2024-09-14 17:25:16 +0200

    Add new container image for file-sync from inside kubernetes
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

---
+++ apps/file-sync/sync.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+config_file="/config/config.yaml"
+known_hosts_file="/root/.ssh/known_hosts"
+source_dir="/files"
+
+readarray hosts < <(yq -r '.hosts[]' "${config_file}")
+post_hook="$(yq -r .post_hook "${config_file}")"
+ssh_key="$(yq -r .ssh_key "${config_file}")"
+target_dir="$(yq -r .target_dir "${config_file}")"
+
+yq -r .known_hosts "${config_file}" >> "${known_hosts_file}"
+
+echo "Found the following files to sync:"
+ls -l "${source_dir}/"
+
+for host in "${hosts[@]}"; do
+    host="${host/$'\n'/}"
+    echo "--- ${host} ---"
+
+    echo "Syncing files"
+
+    rc=0
+    rsync_out="$(rsync --rsync-path="sudo rsync" -r -c -L --out-format='%n%L' -e "ssh -i ${ssh_key}" "${source_dir}"/* "${host}":"${target_dir}"/)" || rc=$?
+
+    if [ $rc -ne 0 ]; then
+        echo "Failed to rsync directory"
+        echo "${rsync_out}"
+        exit $rc
+    fi
+
+    if [ "${rsync_out}" != "" ]; then
+        echo "Changed files:"
+        echo "${rsync_out}"
+        echo "Running post hook"
+        ssh -i "${ssh_key}" "${host}" /bin/bash -c "${post_hook}"
+    else
+        echo "No changes, skipping post hook"
+    fi
+done
