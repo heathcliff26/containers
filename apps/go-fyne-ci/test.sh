533405e10e85957f34bad4dda4586fb8d5bf44a1
Heathcliff <heathcliff@heathcliff.eu>
2026-05-02 16:23:15 +0200

    go-fyne-ci: Switch to support building with goreleaser
    Build fyne apps with goreleaser instead of fyne-cross.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- apps/go-fyne-ci/test.sh
+++ apps/go-fyne-ci/test.sh
@@ -4,24 +4,19 @@ set -ex
 
 base_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)"
 
-export FYNE_CROSS_IMAGE="localhost/go-fyne-ci-test:local-test"
+export BUILDER_IMAGE="localhost/go-fyne-ci-test:local-test"
 
 pushd "${base_dir}" > /dev/null
 
-CONTAINER_ENGINE="podman"
-if command -v docker >/dev/null 2>&1; then
-    CONTAINER_ENGINE="docker"
-fi
-
-"${CONTAINER_ENGINE}" build -t "${FYNE_CROSS_IMAGE}" .
+podman build -t "${BUILDER_IMAGE}" .
 
 tmp_dir="$(mktemp -d /tmp/go-fyne-ci-test.XXXXXXXXXX)"
 
 pushd "${tmp_dir}" > /dev/null
 
-git clone --single-branch --depth 1 https://github.com/heathcliff26/infraspace-savegame-editor.git .
+git clone --single-branch https://github.com/heathcliff26/infraspace-savegame-editor.git .
 
-hack/fyne-cross.sh linux
+make release
 
 popd > /dev/null
 rm -rf "${tmp_dir}"
